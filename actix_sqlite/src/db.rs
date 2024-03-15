c6cd3231400e3459212690fa3b44d197236d2346
akameco <akameco.t@gmail.com>
2024-03-15 18:17:01 +0900

    update

---
+++ actix_sqlite/src/db.rs
@@ -0,0 +1,125 @@
+use std::{thread::sleep, time::Duration};
+
+use actix_web::{error, web, Error};
+use rusqlite::Statement;
+use serde::{Deserialize, Serialize};
+
+pub type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
+pub type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>;
+type WeatherAggResult = Result<Vec<WeatherAgg>, rusqlite::Error>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum WeatherAgg {
+    AnnualAgg { year: i32, total: f64 },
+    MonthAgg { year: i32, month: i32, total: f64 },
+}
+
+#[allow(clippy::enum_variant_names)]
+pub enum Queries {
+    GetTopTenHottestYears,
+    GetTopTenColdestYears,
+    GetTopTenHottestMonth,
+    GetTopTenColdestMonth,
+}
+
+pub async fn execute(pool: &Pool, query: Queries) -> Result<Vec<WeatherAgg>, Error> {
+    let pool = pool.clone();
+
+    let conn = web::block(move || pool.get())
+        .await?
+        .map_err(error::ErrorInternalServerError)?;
+
+    web::block(move || {
+        // 重いクエリを模倣
+        sleep(Duration::from_secs(1));
+
+        match query {
+            Queries::GetTopTenHottestYears => get_hottest_years(conn),
+            Queries::GetTopTenColdestYears => get_coldest_years(conn),
+            Queries::GetTopTenHottestMonth => get_hottest_months(conn),
+            Queries::GetTopTenColdestMonth => get_coldest_months(conn),
+        }
+    })
+    .await?
+    .map_err(error::ErrorInternalServerError)
+}
+
+fn get_hottest_years(conn: Connection) -> WeatherAggResult {
+    let stmt = conn.prepare(
+        "
+    SELECT cast(strftime('%Y', date) as int) as theyear,
+            sum(tmax) as total
+        FROM nyc_weather
+        WHERE tmax <> 'TMAX'
+        GROUP BY theyear
+        ORDER BY total DESC LIMIT 10",
+    )?;
+
+    get_rows_as_annual_agg(stmt)
+}
+
+fn get_coldest_years(conn: Connection) -> WeatherAggResult {
+    let stmt = conn.prepare(
+        "
+      SELECT cast(strftime('%Y', date) as int) as theyear,
+              sum(tmax) as total
+      FROM nyc_weather
+      WHERE tmax <> 'TMAX'
+      GROUP BY theyear
+      ORDER BY total ASC LIMIT 10",
+    )?;
+
+    get_rows_as_annual_agg(stmt)
+}
+
+fn get_rows_as_annual_agg(mut statement: Statement) -> WeatherAggResult {
+    statement
+        .query_map([], |row| {
+            // MEMO: ここのキャストの失敗は実行時エラー
+            Ok(WeatherAgg::AnnualAgg {
+                year: row.get(0)?,
+                total: row.get(1)?,
+            })
+        })
+        .and_then(Iterator::collect)
+}
+
+fn get_hottest_months(conn: Connection) -> WeatherAggResult {
+    let stmt = conn.prepare(
+        "SELECT cast(strftime('%Y', date) as int) as theyear,
+              cast(strftime('%m', date) as int) as themonth,
+              sum(tmax) as total
+      FROM nyc_weather
+      WHERE tmax <> 'TMAX'
+      GROUP BY theyear, themonth
+      ORDER BY total DESC LIMIT 10",
+    )?;
+
+    get_rows_as_month_agg(stmt)
+}
+
+fn get_coldest_months(conn: Connection) -> WeatherAggResult {
+    let stmt = conn.prepare(
+        "SELECT cast(strftime('%Y', date) as int) as theyear,
+              cast(strftime('%m', date) as int) as themonth,
+              sum(tmax) as total
+      FROM nyc_weather
+      WHERE tmax <> 'TMAX'
+      GROUP BY theyear, themonth
+      ORDER BY total ASC LIMIT 10",
+    )?;
+
+    get_rows_as_month_agg(stmt)
+}
+
+fn get_rows_as_month_agg(mut statement: Statement) -> WeatherAggResult {
+    statement
+        .query_map([], |row| {
+            Ok(WeatherAgg::MonthAgg {
+                year: row.get(0)?,
+                month: row.get(1)?,
+                total: row.get(2)?,
+            })
+        })
+        .and_then(Iterator::collect)
+}
--- actix_sqlite/src/main.rs
+++ actix_sqlite/src/main.rs
@@ -1,23 +1,51 @@
-use actix_web::{get, middleware, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer};
 use env_logger;
+use futures_util::future::join_all;
 use log;
 
+mod db;
+use db::{Pool, Queries};
+use r2d2_sqlite::SqliteConnectionManager;
+
 #[get("/asyncio")]
-async fn asyncio_weather() -> impl Responder {
-    let result = vec!["a", "b", "c"];
-    HttpResponse::Ok().json(result)
+async fn asyncio_weather(db: web::Data<Pool>) -> Result<HttpResponse, actix_web::Error> {
+    let result = vec![
+        db::execute(&db, Queries::GetTopTenHottestYears).await?,
+        db::execute(&db, Queries::GetTopTenColdestYears).await?,
+        db::execute(&db, Queries::GetTopTenHottestMonth).await?,
+        db::execute(&db, Queries::GetTopTenColdestMonth).await?,
+    ];
+    Ok(HttpResponse::Ok().json(result))
+}
+
+#[get("/parallel")]
+async fn parallel_weather(db: web::Data<Pool>) -> Result<HttpResponse, actix_web::Error> {
+    let future_result = vec![
+        db::execute(&db, Queries::GetTopTenHottestYears),
+        db::execute(&db, Queries::GetTopTenColdestYears),
+        db::execute(&db, Queries::GetTopTenHottestMonth),
+        db::execute(&db, Queries::GetTopTenColdestMonth),
+    ];
+
+    let result: Result<Vec<_>, _> = join_all(future_result).await.into_iter().collect();
+    Ok(HttpResponse::Ok().json(result.map_err(actix_web::Error::from)?))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
+    let manager = SqliteConnectionManager::file("weather.db");
+    let pool = Pool::new(manager).unwrap();
+
     log::info!("starting HTTP server at http://localhost:8080");
 
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .wrap(middleware::Logger::default())
             .service(asyncio_weather)
+            .service(parallel_weather)
     })
     .bind(("127.0.0.1", 8080))?
     .workers(2)
