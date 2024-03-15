07cabb16e5a5b52b69fcfa6900a175963bc88bf9
akameco <akameco.t@gmail.com>
2024-03-15 16:41:37 +0900

    add logger

--- actix_sqlite/src/main.rs
+++ actix_sqlite/src/main.rs
@@ -1,17 +1,26 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware, App, HttpResponse, HttpServer, Responder};
 use env_logger;
+use log;
 
 #[get("/asyncio")]
 async fn asyncio_weather() -> impl Responder {
-    HttpResponse::Ok().body("ok")
+    let result = vec!["a", "b", "c"];
+    HttpResponse::Ok().json(result)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // env_logger::init_from_env()
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    HttpServer::new(move || App::new().service(asyncio_weather))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(asyncio_weather)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .workers(2)
+    .run()
+    .await
 }
